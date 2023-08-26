cc999c181f2ae522d66698de78acd6607178d877
KuyaAC <aciesnet02@gmail.com>
2023-08-26 17:43:37 +0800

    Start Cloning

---
+++ src/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Homepage() {
+  return (
+    <div className='homepage'>
+        <div className='homepage__nav'></div>
+        <div className='homepage__timeline'></div>
+        <div className='homepage__suggestion'></div>
+    </div>
+  )
+}
+
+export default Homepage
