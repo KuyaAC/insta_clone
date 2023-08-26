90d85b2a4a1b6910fe9b1c0071da77629c081e33
KuyaAC <aciesnet02@gmail.com>
2023-08-26 17:53:11 +0800

    Add Structure

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from "react";
+import "./Homepage.css";
+import Sidenav from "./nav/Sidenav";
+import Timeline from "./timeline/Timeline";
 
 function Homepage() {
   return (
-    <div className='homepage'>
-        <div className='homepage__nav'></div>
-        <div className='homepage__timeline'></div>
-        <div className='homepage__suggestion'></div>
+    <div className="homepage">
+      <div className="homepage__nav"></div>
+      <div className="homepage__timeline"></div>
+      <div className="homepage__suggestion"></div>
     </div>
-  )
+  );
 }
 
-export default Homepage
+export default Homepage;
